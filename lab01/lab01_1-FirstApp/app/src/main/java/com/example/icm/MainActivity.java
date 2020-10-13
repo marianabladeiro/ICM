24e86bdf77e6f42f5e7beaffa59e23b82c17837a
Mariana Ladeiro <marianaladeiro@ua.pt>
2020-10-13 17:17:47 +0100

    add files

---
+++ lab01/lab01_1-FirstApp/app/src/main/java/com/example/icm/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.icm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    private static final String TAG =
+            MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d(TAG, "Creating the URI…");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+}
